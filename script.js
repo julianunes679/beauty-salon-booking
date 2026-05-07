4a212f4a2b2f79c3e7b8dabb75439497efca10de
Julia Alves Nunes <juliaalvesnunes1003@gmail.com>
2026-05-07 17:15:47 +0000

    ajustes

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ const formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  alert("Agendamento realizado com sucesso!");
+  document.querySelector(".card").innerHTML = `
+    <div class="success">
+      <h1>✨ Agendamento enviado!</h1>
+
+      <p>
+        Retornaremos com a confirmação do seu atendimento.
+      </p>
+    </div>
+  `;
 });
